5ee125ef447f6185f6f6aa2908457ea716f58807
basil-yakimov <damselfly@yandex.ru>
2018-08-10 16:38:51 +0300

    DLS co-dependence analysis

---
+++ 04a.codep-dls.r
@@ -0,0 +1,145 @@
+library(codep)
+library(vegan)
+source("R/custom.plot.cdp.r")
+
+load("clean data/int.sc.2018.rda")
+
+ts <- decostand(cts, method = "hellinger")
+h <- decostand(ch, method = "hellinger")
+b <- decostand(cb, method = "hellinger")
+
+ts <- as.matrix(ts)
+h <- as.matrix(h)
+b <- as.matrix(b)
+
+#------------------------------------------#
+
+# Generate an object with spatial variables
+map <- eigenmap(x = 1:nrow(b), weighting = Wf.binary, boundaries = c(0,1))
+#plot(map)
+
+#------------------------------------------#
+
+# Co-dependence analysis of relation between beetle community and woody species
+mca.ts <- MCA(Y = b, X = ts, emobj = map)
+mca.ts.partest <- test.cdp(mca.ts)
+summary(mca.ts.partest)
+
+ind <- colSums(ts > 0) > 9
+
+png("figs/Fig5a.png", width = 800, height = 350)
+par(mar = c(2,11.5,.5,3.5), cex = 1.5)
+plot.cdp(mca.ts.partest, las = 2, col = rev(terrain.colors(256)), ind = ind)
+dev.off()
+
+## Nonparametric testing, similar results, but much longer computation
+#mca.ts.pertest <- permute.cdp(mca.ts)
+#summary(mca.ts.pertest)
+#plot(mca.ts.pertest, las = 2, xlab = "dd")
+
+#mca.ts.pertest$UpYXcb$C[1:10,1L,]
+
+res.ts <- data.frame(mem = names(mca.ts.partest$test$significant$X), 
+                  var = colnames(ts)[mca.ts.partest$test$significant$X],
+                  c = NA, 
+                  phi = mca.ts.partest$test$global[1:length(mca.ts.partest$test$significant$X),2],
+                  p = mca.ts.partest$test$global[1:length(mca.ts.partest$test$significant$X),6])
+coef <- mca.ts.partest$UpYXcb$CM
+for (ii in 1:length(mca.ts.partest$test$significant$X))
+{
+    res.ts$c[ii] <- coef[names(mca.ts.partest$test$significant$X)[ii], mca.ts.partest$test$significant$X[ii]]
+}
+res.ts$c <- round(res.ts$c, dig = 3)
+res.ts$phi <- round(res.ts$phi, dig = 2)
+res.ts$p <- round(res.ts$p, dig = 3)
+
+# Final results table
+res.ts
+
+#------------------------------------------#
+
+# Co-dependence analysis of relation between beetle community and herb species
+mca.h <- MCA(Y = b, X = h, emobj = map)
+mca.h.partest <- test.cdp(mca.h)
+summary(mca.h.partest)
+
+ind <- colSums(h > 0) > 25
+
+png("figs/Fig5b.png", width = 800, height = 450)
+par(mar = c(2,18,.5,3.5), cex = 1.5)
+plot.cdp(mca.h.partest, las = 2, col = rev(terrain.colors(256)), ind = ind)
+dev.off()
+
+## Nonparametric testing, similar results, but much longer computation
+#mca.h.pertest <- permute.cdp(mca.h)
+#summary(mca.h.pertest)
+#plot(mca.h.pertest, las = 2)
+
+#mca.h.pertest$UpYXcb$C[,1L,]
+
+res.h <- data.frame(mem = names(mca.h.partest$test$significant$X), 
+                     var = colnames(h)[mca.h.partest$test$significant$X],
+                     c = NA, 
+                     phi = mca.h.partest$test$global[1:length(mca.h.partest$test$significant$X),2],
+                     p = mca.h.partest$test$global[1:length(mca.h.partest$test$significant$X),6])
+coef <- mca.h.partest$UpYXcb$CM
+for (ii in 1:length(mca.h.partest$test$significant$X))
+{
+    res.h$c[ii] <- coef[names(mca.h.partest$test$significant$X)[ii], mca.h.partest$test$significant$X[ii]]
+}
+
+res.h$c <- round(res.h$c, dig = 3)
+res.h$phi <- round(res.h$phi, dig = 2)
+res.h$p <- round(res.h$p, dig = 3)
+
+#------------------------------------------#
+
+# Constructing a predictor matrix of PCs from woody and herb communities
+
+ts.pca <- rda(ts)
+barplot(ts.pca$CA$eig, col = "tomato")
+abline(h = mean(ts.pca$CA$eig))
+ts.npc <- sum(ts.pca$CA$eig > mean(ts.pca$CA$eig))
+ts.sc <- scores(ts.pca, choices = 1:ts.npc)$sites
+
+h.pca <- rda(h)
+barplot(h.pca$CA$eig, col = "limegreen")
+abline(h = mean(h.pca$CA$eig))
+h.npc <- sum(h.pca$CA$eig > mean(h.pca$CA$eig))
+h.sc <- scores(h.pca, choices = 1:h.npc)$sites
+
+expl <- cbind(ts.sc, h.sc)
+colnames(expl) <- c(paste0("ts", 1:ts.npc), paste0("h", 1:h.npc))
+
+#------------------------------------------#
+
+# Final analysis. Multiscale co-dependence analysis between insect community and phytocoenosis
+mca <- MCA(Y = b, X = expl, emobj = map)
+mca.partest <- test.cdp(mca)
+summary(mca.partest)
+
+png("figs/Fig5c.png", width = 800, height = 600)
+par(mar = c(2,10,.5,3.5), cex = 1.5)
+plot.cdp(mca.partest, las = 2, col = rev(terrain.colors(256)))
+dev.off()
+
+## Nonparametric testing, similar results, but much longer computation
+#mca.pertest <- permute.mca(mca)
+#summary(mca.pertest)
+#plot(mca.pertest, las = 2)
+
+res <- data.frame(mem = names(mca.partest$test$significant$X), 
+                  var = colnames(expl)[mca.partest$test$significant$X],
+                  c = NA, phi = mca.partest$test$global[1:length(mca.partest$test$significant$X),2],
+                  p = mca.partest$test$global[1:length(mca.partest$test$significant$X),6])
+coef <- mca.partest$UpYXcb$CM
+for (ii in 1:length(mca.partest$test$significant$X))
+    res$c[ii] <- coef[names(mca.partest$test$significant$X)[ii], mca.partest$test$significant$X[ii]]
+
+
+res$c <- round(res$c, dig = 3)
+res$phi <- round(res$phi, dig = 2)
+res$p <- round(res$p, dig = 3)
+
+# Final results table
+res
---
+++ R/custom.plot.cdp.r
@@ -0,0 +1,32 @@
+
+#x <- mca.ts.partest
+
+#plot.cdp(x, las = 2)
+#plot(x, las = 2)
+
+plot.cdp <- function (x, col, numscales = 20, ind, col.signif = 2, main = "", ...) {
+    if(missing(col))
+        col <- grey(seq(1, 0, length.out = 256))
+    if(missing(ind)) ind <- rep(T, ncol(x$data$X))
+    mar <- par()$mar
+    z <- log10(x$UpYXcb$CM + 1e-04)[1:numscales, ind]
+    par(mar = c(mar[1L], mar[2L], mar[3L], 0.75), fig = c(0, 0.875 - 0.025 * (mar[4L] - 2.1), 0, 1))
+    image(y = 1L:sum(ind), x = 1L:numscales, z = z,
+          zlim = c(-4, 1e-04), col = col, axes = FALSE, xlab = "", ylab = "",
+          main = main, ...)
+    box(...)
+    axis(1, at = 1L:numscales, labels = 1:numscales, ...)
+    axis(2, at = 1L:sum(ind), labels = colnames(x$data$X)[ind], ...)
+    if (!is.null(x$test$signif)) 
+        rect(xleft = x$test$signif$U - 0.5, xright = x$test$signif$U + 0.5,
+             ybottom = cumsum(ind)[x$test$signif$X] - 0.5, ytop = cumsum(ind)[x$test$signif$X] + 0.5,
+             border = col.signif, density = NULL, ...)
+    par(mar = c(mar[1L], 0.75, mar[3L], mar[4L]), fig = c(0.875 - 0.025 * (mar[4L] - 2.1), 1, 0, 1), new = TRUE)
+    image(z = matrix(seq(-4, 1e-04, length.out = 256), 1L, 256L), x = 0,
+          y = seq(-4, 1e-04, length.out = 256), col = col, axes = FALSE,
+          xlab = "", ylab = "", main = "", ...)
+    box(...)
+    axis(4, labels = 10^seq(-4, 0, 1), at = seq(-4, 0, 1), ...)
+    par(mar = mar, fig = c(0, 1, 0, 1))
+    return(invisible(NULL))
+}
